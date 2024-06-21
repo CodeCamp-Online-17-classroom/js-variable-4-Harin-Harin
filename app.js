ff20a45e65d022bff0b9e0b20e5823e62df182ef
Harin-Mabao <mixharinmabao@gmail.com>
2024-06-21 16:12:50 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+const firstname = "Harin"
+let lastname = "Mabao"
+var nickname = "Mig"
+
+const firstname = "Jaew"
+let lastname = "Jarai"
+var nickname = "JaewJaew"
+
+birthdate = "28/09/1995"
